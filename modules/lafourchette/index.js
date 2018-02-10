2b4beed58be2b01f702c3baf3b4df8eda44dc341
rhoua <r.houaoui@gmail.com>
2018-02-10 22:17:03 +0100

    feat() : scrapping Fork API, access the result of the request and match with a michelin restaurant with the name and the locality

--- modules/lafourchette/index.js
+++ modules/lafourchette/index.js
@@ -1,26 +1,32 @@
 var fs = require('fs');
 var request = require("request");
 var cheerio = require("cheerio");
-
+var results = [];
 var data = fs.readFileSync("../michelin/RestaurantsInfo.json", "utf8");
 var restaurant = data.split("\n");
 var contents = [];
+
 for(var i = 0 ; i <restaurant.length -1 ; i++)
 {
   contents[i] = JSON.parse(restaurant[i]);
 }
 
-function getIDFork()
+function getIDFork(callback)
 {
-  var results = [];
   request({
     uri:"https://m.lafourchette.com/api/restaurant-prediction?name=Auberge_du_Cheval",
   }, function(error , response , body){
     var $ = cheerio.load(body);
     results = JSON.parse($.text().trim());
-    
+    callback();
   });
-  console.log(results);
 }
 
-getIDFork();
+function getIdResults()
+{
+  for(var i = 0 ; i < results.length ; i++)
+  {
+    console.log(results[i].address.address_locality);
+  }
+}
+getIDFork(getIdResults);
