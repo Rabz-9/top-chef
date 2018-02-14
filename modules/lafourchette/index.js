9bb80b29284be1866a6077aff05b6fa7d2c1427d
rhoua <r.houaoui@gmail.com>
2018-02-14 11:31:36 +0100

    feat() : scrapping Fork API , match the michelin restaurants in lafourchette website and we pick the id to find the discount for each restaurant

--- modules/lafourchette/index.js
+++ modules/lafourchette/index.js
@@ -5,28 +5,45 @@ var results = [];
 var data = fs.readFileSync("../michelin/RestaurantsInfo.json", "utf8");
 var restaurant = data.split("\n");
 var contents = [];
+var idFork = 0;
 
-for(var i = 0 ; i <restaurant.length -1 ; i++)
-{
+var test = {
+  "name": "Auberge du Cheval",
+  "street": "47 r. Gisors",
+  "postal_code": 95300,
+  "city": "Pontoise"
+}
+
+for (var i = 0; i < restaurant.length - 1; i++) {
   contents[i] = JSON.parse(restaurant[i]);
 }
 
-function getIDFork(callback)
-{
-  request({
-    uri:"https://m.lafourchette.com/api/restaurant-prediction?name=Auberge_du_Cheval",
-  }, function(error , response , body){
-    var $ = cheerio.load(body);
-    results = JSON.parse($.text().trim());
-    callback();
-  });
-}
-
-function getIdResults()
-{
-  for(var i = 0 ; i < results.length ; i++)
-  {
-    console.log(results[i].address.address_locality);
+function getIDFork() {
+  for (var i = 0; i < contents.length ; i++) {
+    let search = contents[i].name;
+    search = encodeURIComponent(search);
+    var url = "https://m.lafourchette.com/api/restaurant-prediction?name=" + search
+    let postalCode = contents[i].postalCode
+    request({
+      uri: url,
+    }, function(error, response, body) {
+      const $ = cheerio.load(body);
+      results = JSON.parse($.text().trim());
+      idFork = getIdResults(postalCode);
+      fs.appendFileSync("./IdFork.txt", idFork + "\r\n", null, 'utf8', (err) => {
+        if (err) console.log(err)
+      });
+      // ///  idFork = callback(contents[i].postalCode);
+    });
   }
 }
-getIDFork(getIdResults);
+
+function getIdResults(postal_code) {
+  for (var i = 0; i < results.length; i++) {
+    if (results[i].address.postal_code == postal_code) {
+      return results[i].id;
+    }
+  }
+}
+
+getIDFork();
